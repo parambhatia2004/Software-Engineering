b9a66b5c1c01008077881627105a8c3b2a02f83b
MAA02 <mada.amza@gmail.com>
2023-03-06 15:37:01 +0000

    update project for ading monte carlo cost components

--- static/js/addComponent.js
+++ static/js/addComponent.js
@@ -1,26 +1,25 @@
 
-const addBtn = document.querySelector(".add");
-
-const input = document.querySelector(".estimations")
+const addBtns = document.getElementsByClassName("add");
 
 function removeInput(){
-    $.ajax({
+    /*$.ajax({
         url: '/removeCostComponent',
         type: 'post',
         data: {
             projectID : document.getElementById("projectid").value(),
-            name : this.parentElement.childNodes[0].value(),
+            type : this.parentElement.childNodes[0].value(),
+            name : this.parentElement.childNodes[1].value(),
             //use this as id of component for this project
-            absval: this.parentElement.childNodes[1].value(),
-            worst: this.parentElement.childNodes[2].value(),
-            best: this.parentElement.childNodes[3].value(),
-            average: this.parentElement.childNodes[4].value()
+            absval: this.parentElement.childNodes[2].value(),
+            worst: this.parentElement.childNodes[3].value(),
+            best: this.parentElement.childNodes[4].value(),
+            average: this.parentElement.childNodes[5].value()
         },
         success:function(response){
             // ajax processing of data
             this.parentElement.remove();
         }
-    })
+    }) */
       // ajax processing of data
       this.parentElement.remove(); 
 }
@@ -31,19 +30,20 @@ function submitInput(){
         type: 'post',
         data: {
             projectID : document.getElementById("projectid").value(),
-            name : this.parentElement.childNodes[0].value(),
+            type : this.parentElement.childNodes[0].value(),
+            name : this.parentElement.childNodes[1].value(),
             //use this as id of component for this project
-            absval: this.parentElement.childNodes[1].value(),
-            worst: this.parentElement.childNodes[2].value(),
-            best: this.parentElement.childNodes[3].value(),
-            average: this.parentElement.childNodes[4].value()
+            absval: this.parentElement.childNodes[2].value(),
+            worst: this.parentElement.childNodes[3].value(),
+            best: this.parentElement.childNodes[4].value(),
+            average: this.parentElement.childNodes[5].value()
         },
         success:function(response){
         }
     }) 
 }
 
-function addInput(){
+function addInput(item){
     const name = document.createElement("input");
     name.type="text"
     name.placeholder="Name of the component"
@@ -82,7 +82,27 @@ function addInput(){
     const hr = document.createElement("hr")
     hr.className="customHR"
 
+    const budget = document.createElement("input")
+    budget.type ="hidden"
+    budget.value="Budget"
+
+    const time = document.createElement("input")
+    time.type ="hidden"
+    time.value="Time"
+
+    var input
+    if(item===1){
+        input = document.querySelector("#timeEstimation");
+    } else {
+        input = document.querySelector("#budgetEstimation")
+    }
+    
     input.appendChild(flex)
+    if (this.id=="addBudget"){
+        flex.appendChild(budget)
+    } else {
+        flex.appendChild(time)
+    }
     flex.appendChild(name);
     flex.appendChild(absval);
     flex.appendChild(worst);
@@ -93,4 +113,10 @@ function addInput(){
     flex.appendChild(hr);
 }
 
-addBtn.addEventListener("click", addInput);
+var count = addBtns.length;
+
+for(var i = 0; i < count; i++) {
+    var item = addBtns[i];
+    const index = i;
+    item.addEventListener("click", function(){addInput(index)});
+}
